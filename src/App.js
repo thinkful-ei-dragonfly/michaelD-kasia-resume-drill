bdfb0fc57cfaa4b60056770b544c6f5ad3fa3a1e
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-20 14:25:48 -0400

    finished sally resume

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
+/* eslint-disable jsx-a11y/aria-role */
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 
 
 
 function App() {
   return (
-    <div className="App" />
-      <header className="App-header" role='banner'>
-        <h1>'SALLY'S RESUME'</h1>
-        <section role="photo">
-			  <img src="http://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="Sally's Profile Picture'
-			  <h4>Email: <a href="mailto:sallystudent@gmail.com">sallystudent@gmail.com</a></h4>
-			  <h4>Phone: <a href="tel:555-555-5555">(555)-555-5555</a></h4>
-	      </section>
-        </header>
-        </div>
+    <div className="App" >
+      <body className="App-body" role="body" >
+        <main className="App-main" role="main" >
+          <header className="App-header" role='banner'>
+            <h1>'SALLY'S RESUME'</h1>
+            <section role="photo">
+              <img src="https://archive.org/download/nasahqphoto-7316006270/nasahqphoto-7316006270.jpg" width="200" height="200"
+                alt="Sally's Profile Picture" />
+            </section>
+            <section role="contact">
+              <h4>Email: <a href="mailto:sallystudent@gmail.com">sallystudent@gmail.com</a></h4>
+              <h4>Phone: <a href="tel:555-555-5555">(555)-555-5555</a></h4>
+            </section>
+          </header>
+          <section role="Education">
+            <h2><b><u>Education</u></b></h2>
+            <ul>
+              <li>
+                <strong>Bachelor of Arts, Economics</strong>
+              </li>
+            </ul>
+          </section>
+          <section role="experience">
+            <h2><b><u>Professional Experience</u></b></h2>
+            <h3>Full Stack Developer</h3>
+            <em>Google, Sacramento, CA (Jan 2012 – Present)</em>
+            <ul>
+              <li>Code and support product via HTML, CSS, and JS.</li>
+            </ul>
+            <h3>Front Ent Developer</h3>
+            <em>Webcorp, Cherry Hill, NJ (Feb 2011 - Dec 2011)</em>
+            <ul>
+              <li>Rapidiously synergized user-centric architectures.</li>
+              <li>Morphed clicks-and-mortar experiences.</li>
+            </ul>
+          </section>
+        </main>
+        <address>
+          <p><a href="mailto:sallystudent@gmail.com">Contact Me</a></p>
+        </address>
+      </body>
+    </div>
   );
 }
 
