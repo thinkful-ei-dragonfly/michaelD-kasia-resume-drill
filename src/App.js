ac47fb6f10d6680c4a066e7ae5074d81cec96ae7
Kasia <kasia@miroartdesign.com>
2019-05-20 13:45:56 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" />
+      <header className="App-header" role='banner'>
+        <h1>'SALLY'S RESUME'</h1>
+        <section role="photo">
+			  <img src="http://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="Sally's Profile Picture'
+			  <h4>Email: <a href="mailto:sallystudent@gmail.com">sallystudent@gmail.com</a></h4>
+			  <h4>Phone: <a href="tel:555-555-5555">(555)-555-5555</a></h4>
+	      </section>
+        </header>
+        </div>
   );
 }
 
